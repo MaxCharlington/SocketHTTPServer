5e1f86a0d623926070b6c4efccc0cdf13e8e3e25
MaxCharlington <maxcharlington03@yandex.ru>
2021-10-22 04:59:53 +0300

    Refactored logger

--- src/logger.hpp
+++ src/logger.hpp
@@ -6,6 +6,7 @@
 #include <iomanip>
 #include <fstream>
 
+
 // Logger class helper to log nowhere
 struct void_ostream {};
 
@@ -14,33 +15,27 @@ void_ostream& operator<<(void_ostream& l, auto) {
 }
 
 
-template <typename Console = std::ostream&, typename Stream = void_ostream, size_t BUFFSIZE = 1024>
+template <typename Stream1 = std::ostream&, typename Stream2 = void_ostream, size_t BUFFSIZE = 2048>
 class Logger {
-    Console console;
-    Stream stream;
+    Stream1 m_stream1;
+    Stream2 m_stream2;
 
 public:
-    Logger(Console console_ = std::clog, Stream stream_ = void_ostream{}) : console{console_}, stream{stream_} {}
-    Logger(std::ofstream& stream_) : console{std::clog}, stream{stream_} {}
+    Logger(Stream1 console = std::clog, Stream2 stream = void_ostream{}) : m_stream1{console}, m_stream2{stream} {}
+    Logger(std::ofstream& fileStream) : m_stream1{void_ostream{}}, m_stream2{fileStream} {}
 
     template <typename ...T>
     void log(T... args) {
-        (console << ... << args);
-        (stream << ... << args);
-        console << "\n\n";
-        stream << "\n\n";
+        (m_stream1 << ... << args);
+        (m_stream2 << ... << args);
+        m_stream1 << "\n\n";
+        m_stream2 << "\n\n";
     }
 
     template <typename ...T>
-    void log_format(const char* format, T... args) {
+    void log_fmt(const char* format, T... args) {
         static char buf[BUFFSIZE];
         snprintf(buf, BUFFSIZE, format, args...);
         log(buf);
-    }
-
-    // Outputs every step of chaining with newline
-    Logger& operator<<(auto arg) {
-        log(arg);
-        return *this;
     }
 };
