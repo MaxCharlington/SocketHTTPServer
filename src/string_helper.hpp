d280476c7689be2551013ff0285c85ec7612a95c
MaxCharlington <maxcharlington03@yandex.ru>
2021-11-02 01:29:54 +0300

    Removed old StringLike concept and concat hack

--- src/string_helper.hpp
+++ src/string_helper.hpp
@@ -9,24 +9,6 @@
 #include "helpers.hpp"
 
 
-template <typename T>
-concept Stringlike = []
-{
-    using type = remove_all_const_t<std::decay_t<T>>;
-    return std::is_same_v<type, char *> ||
-           std::is_same_v<type, char> ||
-           std::is_same_v<type, std::string> ||
-           std::is_same_v<type, std::string_view>;
-}();
-
-// Hacky way to concat stringlike objects. Update to reduce allocations
-template <Stringlike Op1, Stringlike Op2>
-std::string operator+(Op1 &&op1, Op2 &&op2)
-{
-    return std::string{std::forward<Op1>(op1)} + std::string{std::forward<Op2>(op2)};
-}
-
-
 std::string replace(std::string subject, std::string_view search, std::string_view replace)
 {
     size_t pos = 0;
